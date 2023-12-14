7a990a778097376f883af7949cf40e16101a659b
Arweenn <arwendumont25@outlook.fr>
2023-12-14 14:46:15 -0800

    first version of header file

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sched.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(void);
+void _EndOfFile(char *input, int status);
+
+#endif /*MAIN_H*/
