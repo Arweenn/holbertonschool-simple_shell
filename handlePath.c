5740015ede7c8d76ad7223a9df93c0714819667c
Arweenn <140003082+Arweenn@users.noreply.github.com>
2023-12-19 17:54:45 +0100

    Revert "multiple modifications to handle the path"

--- handlePath.c
+++ handlePath.c
@@ -8,45 +8,48 @@
 
 char *handle_path(char *input)
 {
-	int i;
-	char *env_entry, *token, *result;
+    char **env_var;
 
-	if (strchr(input, '/') != NULL)
-		return (strdup(input));
+    if (strchr(input, '/') != NULL)
+        return strdup(input);
 
-	for (i = 0; environ[i] != NULL; i++)
-	{
-		env_entry = strdup(environ[i]);
-		token = strtok(env_entry, "=");
-		if (strcmp(token, "PATH") == 0)
-		{
-			token = strtok(NULL, "=");
-			token = strtok(token, ":");
-			while (token != NULL)
-			{
-				result = malloc(strlen(token) + strlen(input) + 2);
-				if (result == NULL)
-				{
-					perror("Malloc is NULL");
-					free(env_entry);
-					return (NULL);
-				}
-				sprintf(result, "%s/%s", token, input);
-				if (access(result, X_OK) == 0)
-				{
-					free(env_entry);
-					return (result);
-				}
+    for (env_var = environ; *env_var != NULL; env_var++)
+    {
+        char *env_entry = strdup(*env_var);
+        char *token = strtok(env_entry, "=");
 
-				free(result);
-				token = strtok(NULL, ":");
-			}
-		}
+        if (strcmp(token, "PATH") == 0)
+        {
+            char *path_token = strtok(NULL, "=");
 
-		free(env_entry);
-		i++;
-	}
+            path_token = strtok(path_token, ":");
+            while (path_token != NULL)
+            {
+                char *result = malloc(strlen(path_token) + strlen(input) + 2);
 
-	free(input);
-	return (NULL);
+                if (result == NULL)
+                {
+                    perror("Malloc is NULL");
+                    free(env_entry);
+                    return NULL;
+                }
+
+                sprintf(result, "%s/%s", path_token, input);
+
+                if (access(result, X_OK) == 0)
+                {
+                    free(env_entry);
+                    return result;
+                }
+
+                free(result);
+                path_token = strtok(NULL, ":");
+            }
+        }
+
+        free(env_entry);
+    }
+
+    free(input);
+    return NULL;
 }
