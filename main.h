caf1259be7552ef396f7e49b6b78dcaa707f9368
Arweenn <arwendumont25@outlook.fr>
2023-12-19 06:13:09 -0800

    path prototype added

--- main.h
+++ main.h
@@ -15,5 +15,6 @@ void exec(char **args, char *input);
 void tokenize(char *input, char *args[]);
 void handlePath(const char* path, int length);
 void printEnv(void);
+char *handle_path(char *input);
 
 #endif /*MAIN_H*/
